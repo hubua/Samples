654f161f4de61b04d10521b90c760b957122422b
George Khubua <george.khubua@live.com>
2016-11-18 18:47:19 +0200

    Add Mongo project

---
+++ Mongo/Person.cs
@@ -0,0 +1,78 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mongo
+{
+    public class Person
+    {
+        public Guid UID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [BsonIgnore]
+        public string FullName { get { return $"{FirstName} {LastName}"; } }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        [BsonIgnoreIfNull]
+        public IList<PhoneNumber> PhoneNumbers { get; set; }
+        [BsonExtraElements]
+        public BsonDocument Metadata { get; set; }
+    }
+
+    public class PhoneNumber
+    {
+        public string Number { get; set; }
+        public PhoneType PhoneType { get; set; }
+    }
+
+    public enum PhoneType
+    {
+        Home,
+        Work,
+        Mobile
+    }
+
+    public static class Generator
+    {
+        private static IEnumerable<string[]> _sampledata;
+        private static Random _random;
+
+        static Generator()
+        {
+            var samplefile = System.IO.File.ReadAllLines("sample_data.csv");
+            _sampledata = samplefile.Skip(1).Select(item => item.Split(','));
+            _random = new Random((int)DateTime.Now.Ticks);
+        }
+
+        public static Person GeneratePerson()
+        {
+            var row = _sampledata.ToList()[_random.Next(_sampledata.Count())];
+
+            var result = new Person()
+            {
+                UID = Guid.NewGuid(),
+                FirstName = row[0],
+                LastName = row[1],
+                Email = row[2],
+                Address = row[3],
+                Metadata = new BsonDocument("rel", "metatada")
+            };
+
+            for (int i = 0; i < _random.Next(3); i++)
+            {
+                if (result.PhoneNumbers == null)
+                {
+                    result.PhoneNumbers = new List<PhoneNumber>();
+                }
+                result.PhoneNumbers.Add(new PhoneNumber() { Number = $"1 111 {_random.Next(999).ToString("000")}", PhoneType = (PhoneType)_random.Next(2) });
+            }
+            
+            return result;    
+        }
+    }
+
+}
---
+++ Mongo/Program.cs
@@ -0,0 +1,40 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Collections.Generic;
+
+namespace Mongo
+{
+
+    /*
+     * https://www.tutorialspoint.com/mongodb/mongodb_overview.htm
+     * https://docs.mongodb.com/getting-started/csharp/indexes/
+     * http://mongodb.github.io/mongo-csharp-driver/2.2/getting_started/quick_tour/
+     * http://www.json-generator.com/
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("sample");
+            var collection = database.GetCollection<BsonDocument>("restaraunts");
+
+            var persons = new List<Person>()
+            {
+                Generator.GeneratePerson(),
+                Generator.GeneratePerson(),
+                Generator.GeneratePerson(),
+            };
+            var person1 = persons[0];
+
+            var collectionPerson = database.GetCollection<Person>("Persons");
+            collectionPerson.InsertMany(persons);
+
+            var filter = Builders<Person>.Filter.Eq("UID", person1.UID);
+            var r = collectionPerson.Find(filter).First();
+
+            
+        }
+    }
+}
