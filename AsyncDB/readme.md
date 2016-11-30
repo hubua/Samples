c2b54002faf135029fe21e634295495a4cf32013
George Khubua <george.khubua@live.com>
2016-11-30 18:34:32 +0200

    Add readme.md

---
+++ AsyncDB/readme.md
@@ -0,0 +1,12 @@
+﻿# AsyncDB
+Using Entity Framework asynchronousely
+
+[] Do not use proxies
+[] Use SQL stored procedures
+
+## Links & Docs
+
+- Entity Framework http://msdn.com/data/ef
+- Multiple repositories and UoW http://www.asp.net/mvc/overview/older-versions/getting-started-with-ef-5-using-mvc-4/implementing-the-repository-and-unit-of-work-patterns-in-an-asp-net-mvc-application
+- Not quite http://stackoverflow.com/questions/28543293/entity-framework-async-operation-takes-ten-times-as-long-to-complete
+
--- Mongo/Program.cs
+++ Mongo/Program.cs
@@ -5,17 +5,7 @@ using System.Collections.Generic;
 
 namespace Mongo
 {
-
-    /*
-     * https://www.tutorialspoint.com/mongodb/mongodb_overview.htm
-     * http://mongodb.github.io/mongo-csharp-driver/2.2/getting_started/quick_tour/
-     * http://mongodb.github.io/mongo-csharp-driver/2.2/reference/bson/mapping/polymorphism/
-     * https://docs.mongodb.com/v3.2/tutorial/enable-authentication/
-     * https://docs.mongodb.com/getting-started/csharp/indexes/
-     * https://docs.mongodb.com/manual/tutorial/perform-two-phase-commits/
-     * http://www.json-generator.com/
-     */
-
+    
     class Program
     {
         static void Main(string[] args)
