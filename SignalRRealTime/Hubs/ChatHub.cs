6ec0e909101db0bcaea8372d0651ec9d3f5f1dfa
George Khubua <george.khubua@live.com>
2017-01-25 18:43:54 +0200

    Add SignalR project

---
+++ SignalRRealTime/Hubs/ChatHub.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRRealTime.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+        public void Send(string name, string msg)
+        {
+            //GlobalHost.ConnectionManager.GetHubContext<ChatHub>.Clients.send
+            Clients.All.addNewMessageToPage(name, msg);
+        }
+    }
+}
---
+++ SignalRRealTime/readme.md
@@ -0,0 +1,5 @@
+﻿# SignalR Real Time
+Using SignalR for real time web operations
+
+## Links & Docs
+- Tutorial https://www.asp.net/signalr/overview/getting-started/introduction-to-signalr
